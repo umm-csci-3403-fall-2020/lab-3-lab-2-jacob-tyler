849025d0eebd7efbe06b823274705251945e1257
JacobJenness <jenne077@morris.umn.edu>
2020-09-25 19:19:55 -0500

    Updated array_merge.c (still WIP)
    This commit pushes a few changes to array_merge.c
    Namely, it can now create an array that contains all of the values of every subarray given
    (This runs into errors for large subarrays)

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,32 +1,50 @@
 #include "array_merge.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstring>
 #include "mergesort.h"
 
-/*
+int removeDuplicates(int* array, int size) { //Needs work
+  if (size <= 1)
+    return size;
+
+  int i, j;
+  j = 0;
+  int* tmp;
+  tmp = (int*) calloc(size, sizeof(int));
+
+  for (i=0; i<size-1; i++) {
+    if (array[i] != array[i+1]) {
+      tmp[j] = array[i];
+      j++;
+    }
+  }
+  tmp[j] = array[size-1];
+  j++;
+
+  for (i=0; i<j; i++)
+    array[i] = tmp[i];
+
+  return j;
+}
+
 void printArray(int A[], int size) {
   int i;
   for (i=0; i<size; i++)
     printf("%d ", A[i]);
   printf("\n");
-}*/
+}
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
   int i;
   //int j;
   int arraySize = 0;
-  //int *tmpArray;
-  //int *longArray;
-  //int *result;
-
-  //Sort the arrays
-  for (i=0; i<num_arrays; i++) {
-    mergesort(sizes[i], values[i]);
-  }
+  int *longArray;
 
   //Create a variable that is the size of all the arrays combined
-  for (i=0; i<num_arrays; i++)
+  for (i=0; i<num_arrays; i++) {
     arraySize = arraySize+sizes[i];
+  }
 
   //Check if the array is empty
   if (arraySize <= 0) {
@@ -35,17 +53,33 @@ int* array_merge(int num_arrays, int* sizes, int** values) {
     empty[0] = 0;
     return empty;
   }
-  /*
-  //Create an array that will contain the entries of all arrays combined
-  longArray = calloc(arraySize, sizeof(int));
 
-  i=0;
-  j=0;
-  while (i<num_arrays) {
-    tmpArray = calloc(sizes[i], sizeof(int));
-    for (j=0; j<sizes[i]; j++) {
-      longArray[j]
-    }
-  }*/
+  //Create an array that will contain the entries of all arrays combined
+  longArray =(int*) calloc(arraySize, sizeof(int));
+
+  memcpy(longArray, values[0], sizes[0]*sizeof(int));
+  int appendLocation = sizes[0];
+  for (i=1; i<num_arrays; i++) {
+    memcpy(longArray+appendLocation+1, values[i], sizes[i]*sizeof(int));
+    appendLocation = appendLocation + sizes[i-1];
+  }
+
+  //Sort longArray
+  mergesort(arraySize, longArray);
+
+  //Eliminate Duplicates
+  printf("\nBefore\n");
+  printArray(longArray, arraySize);
+
+  int newArraySize;
+  newArraySize = removeDuplicates(longArray, arraySize);
+  printf("After\n");
+  printArray(longArray, arraySize);
+  printf("newArraySize=%d\n", newArraySize);
+
+  //Count the number of entries
+
+  //Create a new array starting with the size of the array
+
   return sizes;
 }
